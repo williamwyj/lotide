43ead22b86ce41bbacdb6ab23fb9c81049dfa537
William Wang <williamwyj1@gmail.com>
2021-06-21 20:34:09 +0000

    Check if inputed values are equal

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🟢Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🔴Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
