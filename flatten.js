71afa752c6e4bf675ec49441bc5184175c203d84
William Wang <williamwyj1@gmail.com>
2021-06-22 23:54:50 +0000

    first commit flatten.js

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  //Check if array lengths are the same
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  //Since the arrays need to be identical, loop thru both arrays and compare each element
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(arrayA,arrayB) {
+  if (eqArrays(arrayA, arrayB)) {
+    console.log(`🟢Assertion Passed, ${arrayA} === ${arrayB}`);
+  } else {
+    console.log(`🔴Assertion Failed, ${arrayA} !== ${arrayB}`);
+  }
+};
+
+output = [];
+const flatten = function(array) {
+  for (element of array) {
+    if (Array.isArray(element)){
+      for (item of element){
+        output.push(item);
+      }
+    } else {
+      output.push(element);
+    }
+  }
+  return output;
+};
+
+assertArrayEqual(flatten([1,2,[3,4],5,[6], []]), [1,2,3,4,5,6]);
