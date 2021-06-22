4cafae936694c413ca5e4a9a6264ef3dd1292a14
William Wang <williamwyj1@gmail.com>
2021-06-22 22:31:11 +0000

    first commit eqArray.js

---
+++ eqArray.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2){
+  //Check if array lengths are the same
+  if (array1.length !== array2.length){
+    return false;
+  }
+  //Since the arrays need to be identical, loop thru both arrays and compare each element
+  for (let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
