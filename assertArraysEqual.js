240fbd19199bc28a302aeb390f61b17a716ae141
William Wang <williamwyj1@gmail.com>
2021-06-28 21:34:13 +0000

    add module.export for assertArraysEqual and modified eqArray var name to be consistent

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,39 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArray = require('./eqArray')
 
-const eqArrays = function (array1, array2){
-  //Check if array lengths are the same
-  if (array1.length !== array2.length){
-    return false;
-  }
-  //Since the arrays need to be identical, loop thru both arrays and compare each element
-  for (let i = 0; i < array1.length; i++) {
-    if (Array.isArray(array1[i])) {
-      if(!eqArrays(array1[i], array2[i])) {
-        return false;
-      }
-    } else if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArrayEqual = function(arrayA,arrayB) {
-  if(eqArrays(arrayA, arrayB)){
+const assertArraysEqual = function(arrayA,arrayB) {
+  if(eqArray(arrayA, arrayB)){
     console.log(`🟢Assertion Passed, ${arrayA} === ${arrayB}`);
   } else {
     console.log(`🔴Assertion Failed, ${arrayA} !== ${arrayB}`);
   }
 };
 
-assertArrayEqual([[2, 3], [4]], [[2, 3], [4]]) // => true
-
-assertArrayEqual([[2, 3], [4]], [[2, 3], [4, 5]]) // => false
-assertArrayEqual([[2, 3], [4]], [[2, 3], 4]) // => false
+module.exports = assertArraysEqual
 
--- eqArray.js
+++ eqArray.js
@@ -1,4 +1,4 @@
-const eqArrays = function (array1, array2){
+const eqArray = function (array1, array2){
   //Check if array lengths are the same
   if (array1.length !== array2.length){
     return false;
@@ -6,7 +6,7 @@ const eqArrays = function (array1, array2){
   //Since the arrays need to be identical, loop thru both arrays and compare each element
   for (let i = 0; i < array1.length; i++) {
     if (Array.isArray(array1[i])) {
-      if(!eqArrays(array1[i], array2[i])) {
+      if(!eqArray(array1[i], array2[i])) {
         return false;
       }
     } else if(array1[i] !== array2[i]) {
@@ -16,4 +16,4 @@ const eqArrays = function (array1, array2){
   return true;
 }
 
-module.exports = eqArrays;
+module.exports = eqArray
