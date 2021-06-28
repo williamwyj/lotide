3861ea7561ceb9065267bb0f3d22d47b9d5a4ef9
William Wang <williamwyj1@gmail.com>
2021-06-28 20:37:27 +0000

    refactored with module export and moved tests to test file in test folder

--- tail.js
+++ tail.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+//returns the input array with the first element removed and reindexed.
 const tail = function(array) {
   let newArray = [];
   for (let i = 1; i < array.length; i++) {
@@ -15,15 +8,5 @@ const tail = function(array) {
   
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-console.log(tail([1]));
-console.log(tail([]));
+module.exports = tail;
 
