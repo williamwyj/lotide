4614935495dbc571cc921b19da639e18e38edaae
William Wang <williamwyj1@gmail.com>
2021-06-29 04:13:52 +0000

    refactored to use mocha and chai test

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,8 +1,20 @@
-const assertEqual = require('../assertEqual')
-const eqArray = require('../eqArray')
+const assert = require('chai').assert;
+const eqArray = require('../eqArray');
 
-assertEqual(eqArray([5,6,7],[5,6,7]), true);
-assertEqual(eqArray(["Hello", "Lighthouse", "Labs"],["Hello", "Lighthouse", "Labs"]), true);
-assertEqual(eqArray(["Hello", "Lighthouse", "Labs"],["Hello", "Lighthouse"]), false);
-assertEqual(eqArray([1,[2,3,4],[2,[2,3],4],1],[1,[2,3,4],[2,[2,3],4],1]), true);
-assertEqual(eqArray([1,[2,3,4],[2,[2,3],4],1],[1,[2,3,4],[2,[2,4],4],1]), false);
+describe('eqArray#', () => {
+  it('compare two input arrays of numbers, true if the same', () => {
+    assert.deepStrictEqual(eqArray([5,6,7],[5,6,7]), true);
+  });
+  it('compare two input arrays of strings, true if the same', () => {
+    assert.deepStrictEqual(eqArray(["Hello", "Lighthouse", "Labs"],["Hello", "Lighthouse", "Labs"]), true);
+  });
+  it('compare two input arrays of string, false if not the same', () => {
+    assert.deepStrictEqual(eqArray(["Hello", "Lighthouse", "Labs"],["Hello", "Lighthouse"]), false);
+  });
+  it('compare two input arrays of numbers, with elements as arrays, true if the same', () => {
+    assert.deepStrictEqual(eqArray([1,[2,3,4],[2,[2,3],4],1],[1,[2,3,4],[2,[2,3],4],1]), true);
+  });
+  it('compare two input arrays of numbers, with elements as arrays, false if not the same', () => {
+    assert.deepStrictEqual(eqArray([1,[2,3,4],[2,[2,3],4],1],[1,[2,3,4],[2,[2,4],4],1]), false);
+  });
+});
